87ef3277c40388ece53d0b532b01d88ba5e12601
kipyek <deniskipyegon88@gmail.com>
2023-03-10 21:08:11 +0300

    Dashboard Screens

---
+++ src/Dashboard/Continents/Continents.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Continents = () => {
+  return (
+    <View>
+      <Text>Continents</Text>
+    </View>
+  )
+}
+
+export default Continents
+
+const styles = StyleSheet.create({})
